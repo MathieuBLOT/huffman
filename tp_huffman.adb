be9a221d94b86431e737b145449fb5e2844050f9
Mathieu <mathieu.blot@phelma.grenoble-inp.fr>
2014-12-02 17:04:43 +0100

    Lit_Huffman a disparu ? -> Il s'agissait de lire l'arbre dans l'entête... NEED

--- tp_huffman.adb
+++ tp_huffman.adb
@@ -1,6 +1,8 @@
-with Ada.Text_Io; use Ada.Text_Io;
-with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Text_Io, Ada.Command_Line, Ada.Streams.Stream_IO;
+use Ada.Text_Io, Ada.Command_Line, Ada.Streams.Stream_IO;
 
+with huffman;
+use huffman;
 
 procedure tp_huffman is
 
@@ -9,9 +11,19 @@ procedure tp_huffman is
 ------------------------------------------------------------------------------
 
 	procedure Compresse(Nom_Fichier_In, Nom_Fichier_Out : in String) is
+		H : Arbre_Huffman;
+		Nb_Octets_Ecrits : Natural := 0;
+		S : Stream_Access;
+		Fichier_Out: File_Type;
 	begin
-		-- A COMPLETER!
-		return;
+		Create(Fichier_Out, Out_File, Nom_Fichier_Out);
+		S := Stream(Fichier_Out);
+
+		H := Cree_Huffman(Nom_Fichier_In);
+
+		Nb_Octets_Ecrits := Ecrit_Huffman(H, S);
+
+		Put_Line("# " & Natural'Image(Nb_Octets_Ecrits) & " octets ont été écrits en mémoire (Huffman Tree) #");
 	end Compresse;
 
 
@@ -21,9 +33,18 @@ procedure tp_huffman is
 ------------------------------------------------------------------------------
 
 	procedure Decompresse(Nom_Fichier_In, Nom_Fichier_Out : in String) is
+		H : Arbre_Huffman;
+		S_In : Stream_Access;
+		Fichier_In: File_Type;
+		S_Out : Stream_Access;
+		Fichier_Out: File_Type;
 	begin
-		-- A COMPLETER!
-		return;
+		Create(Fichier_In, In_File, Nom_Fichier_In);
+		S_In := Stream(Fichier_In);
+		Create(Fichier_Out, Out_File, Nom_Fichier_Out);
+		S_Out := Stream(Fichier_Out);
+
+		H := Lit_Huffman(S_In);
 	end Decompresse;
 
 
